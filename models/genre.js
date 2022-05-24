ef0408f763cadf51d3465c11ad72afc4adce183b
2471186676 <alex145561625@gmail.com>
2022-05-24 11:29:22 +0800

    first commit

---
+++ models/genre.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var genreSchema = new Schema({
+	name: { type: String, require: true, minlength: 3, maxlength: 100 },
+});
+
+genreSchema.virtual("url").get(function () {
+	return "/catalog/genre/" + this._id;
+});
+
+module.exports = mongoose.model("Genre", genreSchema);
